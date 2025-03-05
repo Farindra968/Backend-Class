8a76075c6a3c6fba8e665b8254c17e278294e1ec
Farindra968 <farindrabahadurbhandari@gmail.com>
2025-03-05 23:20:25 +0545

    Update syncFile.js to enable console logging for file operations and fix appendFileSync typo

--- module/filesystem/syncFile.js
+++ module/filesystem/syncFile.js
@@ -3,23 +3,23 @@ import fs from 'fs';
 
 // readFileSync
 const data = fs.readFileSync('file.txt', 'utf8');
-// console.log(data);
+console.log(data);
 
 // fs.writeFileSync
 const write = fs.writeFileSync('file.txt', 'Hello');
-// console.log(write)
+console.log(write)
 
 // fs.appendFikeSync()
 const update = fs.appendFileSync('file.txt', ', We are learning NodeJS fie System Module');
-// console.log(update);
+console.log(update);
 
 // fs.unlink()
-// const unlink = fs.unlinkSync("data.txt",)
-// console.log(unlink);
+const unlink = fs.unlinkSync("data.txt",)
+console.log(unlink);
 
 // fs.mkdirSync()
-// const dir = fs.mkdirSync('path')
-// console.log(dir)
+const dir = fs.mkdirSync('path')
+console.log(dir)
 
 // fs.existsSync()
 const exist = fs.existsSync("data")
